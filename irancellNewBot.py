30faa62e310c8b714106973e33f18236b954989e
AliMi00 <alimobinidev@gmail.com>
2020-05-24 17:34:19 +0430

    fix otp option add venv

--- irancellNewBot.py
+++ irancellNewBot.py
@@ -4,7 +4,12 @@ from selenium.webdriver.common.keys import Keys
 class irancellBot():
     def __init__(self):
         #open chrome
-        self.driver = webdriver.Chrome()
+        option = webdriver.ChromeOptions()
+        option.add_argument('--ignore-certificate-errors')
+
+        chromePath = 'G:\drive_D\Python\seleniumVenv\selenium-try\IrancellAutoReg\chromedriver.exe'
+
+        self.driver = webdriver.Chrome(chromePath,chrome_options=option)
 
     #login 
     def login(self):
@@ -16,10 +21,12 @@ class irancellBot():
         pasOtp = self.driver.find_element_by_name('otp')
 
         otp = input('insert and enter the otp : ')
+        username = input('Enter username :')
+        password = input('enter pass word : ')
 
         #pass data
-        pas.send_keys('_e41_e41')
-        user.send_keys('ehsan.mob')
+        pas.send_keys(password)
+        user.send_keys(username)
         pasOtp.send_keys(otp)
 
         loginSub = self.driver.find_element_by_xpath('//*[@id="loginform"]/div[5]/div/div/button')
