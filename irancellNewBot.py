a481df034c2e03fb5baacf510fb021e71ccb65e0
AliMi00 <alimobinidev@gmail.com>
2020-05-25 19:53:59 +0430

    new web set elm add pandas and xlrd lib to venv

--- irancellNewBot.py
+++ irancellNewBot.py
@@ -37,6 +37,82 @@ class irancellBot():
 
         
 
+    def addToChart(self):
+        self.driver.get('https://sale.irancell.ir/Product/5b28b561d0b9643bbc68fb46/Prepaid%20SP%20SIM')
+        btnSim = self.driver.find_element_by_xpath('//*[@id="page-wrapper"]/div[2]/div[1]/div[3]/div/div/div/div/div[2]/div/button')
+        btnSim.click()
+
+        #click cart
+        cartElm = self.driver.find_element_by_xpath('//*[@id="wrapper"]/div/nav/div/ul[2]/li[2]/a/i')
+        #cartElm.click()
+        
+        CartGo = self.driver.find_element_by_xpath('//*[@id="wrapper"]/div/nav/div/ul[2]/li[2]/ul/li[2]/div/a/div[3]/span')
+        CartGo.click()
+
+        simSerial = self.driver.find_element_by_xpath('//*[@id="kit_number0"]')
+        simSerial.send_keys('')
+
+        self.driver.find_element_by_xpath('//*[@id="page-wrapper"]/div[2]/div[1]/div[3]/div/div/div/div/table/tbody/tr[1]/td[2]/div[1]/span/button').click()
+
+        self.driver.find_element_by_xpath('//*[@id="page-wrapper"]/div[2]/div[1]/div[3]/div/div/div/div/table/tbody/tr[1]/td[2]/div[2]/span/span/a').click()
+
+        simMs = self.driver.find_element_by_xpath('//*[@id="page-wrapper"]/div[2]/div[1]/div[3]/div/div/div/div/table/tbody/tr[1]/td[2]/div[2]/span/input')
+        simMs.send_keys('')
+
+        self.driver.find_element_by_xpath('//*[@id="page-wrapper"]/div[2]/div[1]/div[3]/div/div/div/div/table/tbody/tr[1]/td[2]/div[2]/span/button').click()
+
+        self.driver.find_element_by_xpath('//*[@id="page-wrapper"]/div[2]/div[1]/div[3]/div/div/div/div/button[1]').click()
+
+        # customer info 
+
+        n_id = self.driver.find_element_by_name('nID')
+        n_id.send_keys()
+        
+        first_name = self.driver.find_element_by_name('first_name')
+        first_name.send_keys()
+
+        last_name = self.driver.find_element_by_name('last_name')
+        last_name.send_keys()
+
+        brith_date = self.driver.find_element_by_xpath('//*[@id="undefined-1370-06-06--39074"]')
+        brith_date.send_keys()
+
+        father_name = self.driver.find_element_by_name('father_name')
+        father_name.send_keys('')
+
+        identity_code = self.driver.find_element_by_name('identity_code')
+        identity_code.send_keys('0')
+
+        gender = self.driver.find_element_by_xpath('//*[@id="select-gender"]')
+        gender.click()
+
+        man = self.driver.find_element_by_xpath('//*[@id="menu-gender"]/div[2]/ul/li[2]').click()
+        female = self.driver.find_element_by_xpath('//*[@id="menu-gender"]/div[2]/ul/li[3]')
+
+        email = self.driver.find_element_by_name('email')
+        email.send_keys('a@a.com')
+
+        mobile = self.driver.find_element_by_name('mobile')
+        mobile.send_keys('')
+
+        tel_code = self.driver.find_element_by_name('tel_code')
+        tel_code.send_keys('')
+
+        tel = self.driver.find_element_by_name('tel')
+        tel.send_keys('')
+
+        postal_code = self.driver.find_element_by_name('postal_code')
+        postal_code.send_keys('')
+
+        address = self.driver.find_element_by_name('address')
+        address.send_keys('خ')
+
+        building_number = self.driver.find_element_by_name('building_number')
+        building_number.send_keys('0')
+
+
+
+        
 
 
 
